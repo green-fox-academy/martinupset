de569fb22780eb4f07e6459f4c91115638903aec
martinupset <Martin_Ma@epam.com>
2019-10-10 23:39:38 +0800

    finish candy

--- epam-academy/week03/Thur/candy-game/game.js
+++ epam-academy/week03/Thur/candy-game/game.js
@@ -5,3 +5,48 @@
 //   - Use the 🍭 emoji to display the lollipops you have
 // - Display the candy producton rate in the `Candies / Second` row
 // - If you press the "make candy rain" button, the candy generation should speed up 10x
+
+let lollipopemj = '🍭'
+let candiescount = 10.000
+let lolcount = 3
+let up = 1
+
+
+let candies = document.querySelector('.candies')
+let lollipop = document.querySelector('.lollypops')
+let speed = document.querySelector('.speed')
+
+let candiesbtn = document.querySelector('.create-candies')
+let buybtn = document.querySelector('.buy-lollypops')
+let rainbtn = document.querySelector('.candy-machine')
+
+candies.textContent = candiescount
+
+const createCandies = () =>{
+    candiescount ++
+    candies.textContent = candiescount
+}
+
+candiesbtn.addEventListener('click', createCandies)
+
+const createlol = () => {
+    lollipop.textContent += lollipopemj
+    lolcount++
+}
+
+buybtn.addEventListener('click', createlol)
+
+const generatecandy = () =>{
+    let rate = Math.floor(lolcount/10) * up
+    candiescount += rate
+    candies.textContent = candiescount
+    speed.textContent = rate
+}
+
+window.setInterval(generatecandy, 1000)
+
+const goup = () => {
+    up = 10
+}
+
+rainbtn.addEventListener('click', goup)
