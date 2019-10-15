13532c1df6732bb069ba4baa62dd266de665df40
martinupset <Martin_Ma@epam.com>
2019-10-16 00:33:57 +0800

    finish helloword n work for last day

---
+++ epam-academy/week04/day02/helloworld/src/component/error.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import {BrowserRouter as Router,
+Switch,
+Route,
+Link,
+useParams} from 'react-router-dom'
+
+function App(){
+    return(
+        <Router>
+            <div>
+                <ul>
+                    <li>
+                        <Link to = '/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to = '/error/:code'>Error code</Link>
+                    </li>
+                    <li>
+                        <link to = '/:any'>Not implemented yet</link>
+                    </li>
+                </ul>
+
+                <Switch>
+                    <Route exact path = '/'>
+                        <Home/>
+                    </Route>
+                    <Route exact path = '/error'>
+                        <Error/>
+                    </Route>
+                    <Route path = '/error/:code'>
+                        <ErrorCode/>
+                    </Route>
+                    <Route path = '/:any'>
+                        <Imp/>
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    )
+}
+
+function Home(){
+    return(
+        <div>
+            <h2>Hello world</h2>
+        </div>
+    )
+}
+
+function Error(){
+    return(
+        <div>
+            <h2>something bad happen</h2>
+        </div>
+    )
+}
+
+function ErrorCode(){
+    let {code} = useParams();
+    return(
+        <div>
+            <h2>Error:{code}</h2>
+        </div>
+    )
+}
+
+function Imp(){
+    return(
+        <div>
+            <h2>This page is not imp</h2>
+        </div>
+    )
+
+}
+export default App;
