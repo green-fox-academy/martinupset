bca17ae07f0bead0c08406edb41f30d0b263aae0
martinupset <Martin_Ma@epam.com>
2019-10-18 13:02:01 +0800

    basic app

--- epam-academy/week04/project/gallery/src/component/buttonline.js
+++ epam-academy/week04/project/gallery/src/component/buttonline.js
@@ -1,9 +1,28 @@
 import React from 'react'
 import './app.css'
 
-class ButtonLine extends React.Component{
+class App extends React.Component{
     constructor(){
         super()
+        this.state = {
+            imgnow: ''
+        }
+    }
+
+    changefn = (img) => {
+        this.setState({imgnow: img})
+        console.log(this.state.imgnow)
+    }
+
+    render(){
+        return (<><ButtonLine fn = {this.changefn}/>
+                <Imgspace value = {this.state.imgnow} /></>)
+    }
+}
+
+class ButtonLine extends React.Component{
+    constructor(props){
+        super(props)
         this.state = {
             buttonimg : ['galleryimg/anni.jpg', 'galleryimg/daomei.jpg', 'galleryimg/eyu.jpg', 'galleryimg/lubu.jpg', 'galleryimg/renma.jpg', 'galleryimg/ruiwen.jpg', 'galleryimg/wukong.jpg', 'galleryimg/xiazi.jpg']
         }
@@ -14,12 +33,20 @@ class ButtonLine extends React.Component{
         
     }
     render(){
-        return (<li className = 'btnline'>
+        return (<li className = 'btnline' key ='aaa'>
             {this.state.buttonimg.map((element) => { 
-              return  <img src = {element} className = 'imgsize' key = {element} alt = {element} onMouseOver = {()=>{this.handleOnMouse(element.key)}}></img>
+              return  <img src = {element} className = 'imgsize' key = {element} alt = {element} onClick = { (event) => this.props.fn(event.target.alt)}></img>
             })}
         </li>)
     }
 }
 
-export default ButtonLine
+class Imgspace extends React.Component{
+    render(){
+        // return (<p>{this.props.value}</p>)
+        return (<div className = 'bigimgsize'><img className = 'bigimgsize' src = {this.props.value} key = {this.props.value} alt = {1}/></div>)
+        
+    }
+}
+
+export default App
--- epam-academy/week04/project/gallery/src/index.js
+++ epam-academy/week04/project/gallery/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ButtonLine from './component/buttonline.js'
+import App from './component/buttonline.js'
 
 
-ReactDOM.render(<ButtonLine />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
