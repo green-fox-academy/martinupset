31d4e41e38ad343b426812388979ef115c865c78
Martinupset <1023090715@qq.com>
2020-12-04 10:22:09 +0800

    day-03&part of day-04

---
+++ java-microservice-academy/week-01/day-02/src/com/company/_01HelloMe.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class _01HelloMe {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Me!");
+    }
+}
---
+++ java-microservice-academy/week-01/day-03/src/com/company/_01third.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class _01third {
+    public static void main(String[] args) {
+        int[] number = {4,5,6,7};
+        System.out.println(number[3]);
+    // - Create an array variable named `numbers`
+    //   with the following content: `[4, 5, 6, 7]`
+    // - Print the third element of `numbers`
+    }
+}
