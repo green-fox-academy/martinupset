5f82268ee182997e8de83defbf6afed395a9e0fe
Martinupset <1023090715@qq.com>
2020-12-07 17:19:26 +0800

    day-05

---
+++ java-microservice-academy/week-01/day-05/src/com/company/_01Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class _01Main {
+    public static void main(String[] args){
+        _01PostIt orange = new _01PostIt("orange", "Idea1","blue");
+        _01PostIt pink = new _01PostIt("pink", "Awesome", "black");
+        _01PostIt yellow = new _01PostIt("yellow", "Superb!", "green");
+    }
+}
---
+++ java-microservice-academy/week-01/day-05/src/com/company/_01PostIt.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class _01PostIt {
+    String backgroundColor;
+    String text;
+    String textColor;
+
+    public _01PostIt(String backgroundColor, String text, String textColor){
+        this.backgroundColor = backgroundColor;
+        this.text = text;
+        this.textColor = textColor;
+    }
+}
